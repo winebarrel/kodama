17d40b6e8d8dc22a392c8a2566d7bd74cc53ff99
winebarrel <sugawara@winebarrel.jp>
2025-12-13 21:44:54 +0900

    Add empty handler

--- go.mod
+++ go.mod
@@ -3,3 +3,12 @@ module github.com/winebarrel/kodama
 go 1.25.3
 
 require github.com/alecthomas/kong v1.13.0
+
+require (
+	github.com/miekg/dns v1.1.69 // indirect
+	golang.org/x/mod v0.30.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/tools v0.39.0 // indirect
+)
--- server.go
+++ server.go
@@ -1,12 +1,36 @@
 package kodama
 
-type Server struct{}
+import (
+	"fmt"
+
+	"github.com/miekg/dns"
+	"golang.org/x/sync/errgroup"
+)
+
+type Server struct {
+	*Options
+}
 
 func NewServer(options *Options) *Server {
-	server := &Server{}
+	server := &Server{
+		Options: options,
+	}
+
 	return server
 }
 
 func (svr *Server) Start() error {
-	return nil
+	addr := fmt.Sprintf(":%d", svr.Port)
+	handler := &Handler{}
+	eg := &errgroup.Group{}
+
+	for _, net := range []string{"udp", "tcp"} {
+		eg.Go(func() error {
+			s := &dns.Server{Addr: addr, Net: net, Handler: handler}
+			defer s.Shutdown()
+			return s.ListenAndServe()
+		})
+	}
+
+	return eg.Wait()
 }
